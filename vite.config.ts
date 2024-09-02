fd01e2016e18e52e175ecd2e1ea4dd8496435026
Aleksey Zayachnikov <lekseff@gmail.com>
2024-09-02 16:08:48 +0300

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     vueDevTools(),
     vuetify({ styles: { configFile: 'src/app/plugins/vuetify/settings.scss' } }),
   ],
+  build: {
+    outDir: "docs"
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
